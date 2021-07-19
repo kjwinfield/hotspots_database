9ccf77fbd97ab90dcca59e52bf3822b491082d55
kjwinfield <katherine.winfield@addenbrookes.nhs.uk>
2021-07-19 13:45:18 +0100

    now displays gene name, chromosome, and start and end positions

--- hotspots/hotspots/hotspots_database/views.py
+++ hotspots/hotspots/hotspots_database/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
-from django_tables2 import SingleTableView, RequestConfig
-from django_tables2 import MultiTableMixin
+from django_tables2 import SingleTableView
 from django.views.generic.base import TemplateView
-from .tables import CompleteTable
 from .models import (
                     GeneName,
                     GrCh37,
@@ -27,36 +22,48 @@ class GeneListView(SingleTableView):
     template_name='hotspots_database/genomic_positions.html'
 
 class GRCh37View(TemplateView):
-    #table_class = CompleteTable
     template_name = 'hotspots_database/multiple_table_template.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         data = []
-        queryset_chr = Chromosome.objects.all()
-        for chrom in queryset_chr:
-            queryset_grch37 = GrCh37.objects.filter(hotspots__chromosome_id=chrom.chromosome_id)
-
-            for grch37_coor in queryset_grch37:
-                data.append([chrom.chromosome, grch37_coor.genomic_position_start_37, grch37_coor.genomic_position_end_37])
+        queryset_positions = GrCh37.objects.all()
+        for position in queryset_positions:
+            list1 =[]
+            queryset_chrom = Chromosome.objects.filter(hotspots__grch37_id=position.grch37_id)
+            for chromosome in queryset_chrom:
+                list1.append(chromosome.chromosome)
+                list1.append(position.genomic_position_start_37)
+                list1.append(position.genomic_position_end_37)
+                query_gene_2 = GeneName.objects.filter(hotspots__grch37_id=position.grch37_id)
+                #print(gene_name
+                for each_query in query_gene_2:
+                    list1.append(each_query.gene_name)
+            data.append(list1)
 
         context["data"] = data
         return context
 
 
 class GRCh38View(TemplateView):
-    #table_class = CompleteTable
     template_name = 'hotspots_database/multiple_table_template.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         data = []
-        queryset_chr = Chromosome.objects.all()
-        for chrom in queryset_chr:
-            queryset_grch38 = GrCh38.objects.filter(hotspots__chromosome_id=chrom.chromosome_id)
-
-            for grch37_coor in queryset_grch38:
-                data.append([chrom.chromosome, grch37_coor.genomic_position_start_38, grch37_coor.genomic_position_end_38])
+        queryset_positions = GrCh38.objects.all()
+        for position in queryset_positions:
+            list1 =[]
+            queryset_chrom = Chromosome.objects.filter(hotspots__grch38_id=position.grch38_id)
+            for chromosome in queryset_chrom:
+                list1.append(chromosome.chromosome)
+                list1.append(position.genomic_position_start_38)
+                list1.append(position.genomic_position_end_38)
+                query_gene_2 = GeneName.objects.filter(hotspots__grch38_id=position.grch38_id)
+                #print(gene_name
+                for each_query in query_gene_2:
+                    list1.append(each_query.gene_name)
+            data.append(list1)
 
         context["data"] = data
         return context
